deb1c5e415f7f624411bd306704cfaff30ae316c
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-31 16:41:44 +0000

    Added tests that reading in a Fraction was sucessful

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -4,6 +4,11 @@ public class FractionCalculatorTest{
     public static void main (String[] args)
     {
     
+    FractionCalculator fc = new FractionCalculator();
+    
+    if (!fc.evaluate(new Fraction(1,2), "1/3").equals(new Fraction(1,3))) System.out.println("Read fraction test 1 failed " + fc.evaluate(new Fraction(1,2), "1/3").toString());
+    if (!fc.evaluate(new Fraction(1,2), "1").equals(new Fraction(1,1))) System.out.println("Read fraction test 2 failed " + fc.evaluate(new Fraction(1,2), "1").toString());
+    
     }
 
 }
