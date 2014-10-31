703cb048550a733329ad8f7b3a20e4d22687ba06
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-31 16:18:41 +0000

    Cleared up a few bugs and added first attempt of reading in a Fraction form a string

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -9,12 +9,46 @@ public class FractionCalculator{
         currentValue = new Fraction(0,1);
         currentOp = Operator.NONE;
         
-        callAbs     = {"a", "A", "abs"};
-        callNegate  = {"n", "N", "neg"};
-        callClear   = {"c", "C", "clear"};
-        callQuit    = {"q", "Q", "quit"};
+        callAbs = new String[]{"a", "A", "abs"};
+        callNegate = new String[]{"n", "N", "neg"};
+        callClear = new String[] {"c", "C", "clear"};
+        callQuit = new String[] {"q", "Q", "quit"};
     }
 
+    public Fraction evaluate(Fraction current, String input)
+    {
+ 
+    
+            if (Character.isDigit(input.charAt(0))) //next input is a number
+            {
+                int i=0;
+                String numString="", denString="";
+                
+                while (Character.isDigit(input.charAt(i)))
+                {
+                    numString = input.substring(0,i+1);
+                    i++;
+                }
+                
+                if (input.charAt(i+1) == '/')
+                {
+                    int start = i+2;
+                    while(Character.isDigit(input.charAt(i+2))) //assuming correct input
+                    {
+                        denString = input.substring(start,i+3);
+                        i++;
+                    }
+                }
+            
+                int num = Integer.parseInt(numString);
+                int den = denString.equals("") ? 1 : Integer.parseInt(denString);
+                
+                return new Fraction(num,den);
+            
+            }
+        
+        return null;
+    }
 
     public static void main (String[] args)
     {
@@ -23,6 +57,6 @@ public class FractionCalculator{
 
 }
 
-public enum Operator{
+enum Operator{
     NONE, ADD, SUBSTRACT, MULTIPLY, DIVIDE
 }
