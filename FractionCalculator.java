f0b0bfbe1830868c02c69e23b6b3995d429c628b
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-31 14:08:16 +0000

    Added constructor and variables to be stored in the class

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -1,8 +1,28 @@
 public class FractionCalculator{
 
+    private Fraction currentValue;
+    private Operator currentOp;
+    private String[] callAbs, callNegate, callClear, callQuit;
+    
+    public FractionCalculator()
+    {
+        currentValue = new Fraction(0,1);
+        currentOp = Operator.NONE;
+        
+        callAbs     = {"a", "A", "abs"};
+        callNegate  = {"n", "N", "neg"};
+        callClear   = {"c", "C", "clear"};
+        callQuit    = {"q", "Q", "quit"};
+    }
+
+
     public static void main (String[] args)
     {
     
     }
 
+}
+
+public enum Operator{
+    NONE, ADD, SUBSTRACT, MULTIPLY, DIVIDE
 }
