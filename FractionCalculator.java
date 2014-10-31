534ca73604f572bfe49aa21695e5e3d7827582f3
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-31 13:51:12 +0000

    First Commit

---
+++ FractionCalculator.java
@@ -0,0 +1,8 @@
+public class FractionCalculator{
+
+    public static void main (String[] args)
+    {
+    
+    }
+
+}
---
+++ FractionCalculatorTest.java
@@ -0,0 +1,9 @@
+
+public class FractionCalculatorTest{
+
+    public static void main (String[] args)
+    {
+    
+    }
+
+}
