97dc0c8fdd475cc09ae069b59320c76de89cdbd2
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-31 11:14:18 +0000

    Added test to 'add' method@

--- FractionTest.java
+++ FractionTest.java
@@ -10,13 +10,18 @@ Fraction f = new Fraction(3,10);
 Fraction g = new Fraction(1,2);
 Fraction h = new Fraction(3,5);
 if (!f.equals(g.multiply(h))) System.out.println("Multiply failed");
+
 // test equals
 test(new Fraction(1, 2),new Fraction(1, 2),"error test 1");
 test(new Fraction(1, 2),new Fraction(3, 6),"error test 2");
 test(new Fraction(-1, 2),new Fraction(1, -2),"error test 3");
 test(new Fraction(-1, -2),new Fraction(1, 2),"error test 4");
 test(new Fraction(4, -8),new Fraction(1, -2),"error test 5");
-// extend with extra tests
+
+// test addition
+Fraction i = new Fraction(8,10);
+if (!i.equals(f.add(g))) System.out.println("Addition failed");
+
 }
 static void test(Fraction f1, Fraction f2, String msg){
 if (! f1.equals(f2))
