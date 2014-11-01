7d3128c860b7d5cced676b7352dc5cb119b05e7f
Paul Day <pday02@mail.bkk.ac.uk>
2014-11-01 12:24:13 +0000

    Changed approach significantly for reading in input to make it clearer, and easier to expand upon full method use.

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -2,7 +2,7 @@ public class FractionCalculator{
 
     private Fraction currentValue;
     private Operator currentOp;
-    private String[] callAbs, callNegate, callClear, callQuit;
+    private String[] callAbs, callNegate, callClear, callQuit, callAdd, callSubtract, callDivide, callMultiply;
     
     public FractionCalculator()
     {
@@ -13,41 +13,50 @@ public class FractionCalculator{
         callNegate = new String[]{"n", "N", "neg"};
         callClear = new String[] {"c", "C", "clear"};
         callQuit = new String[] {"q", "Q", "quit"};
+        callAdd = new String[] {"+"};
+        callSubtract = new String[] {"-"};
+        callMultiply = new String[] {"*"};
+        callDivide = new String[] {"/"};
     }
 
     public Fraction evaluate(Fraction current, String input)
     {
- 
-    
-            if (Character.isDigit(input.charAt(0))) //next input is a number
-            {
-                int i=0;
-                String numString="", denString="";
-                
-                while (i < input.length() && Character.isDigit(input.charAt(i)))
-                {
-                    numString = input.substring(0,i+1);
-                    i++;
-                }
-                
-                if (i < input.length() && input.charAt(i) == '/')
-                {
-                    int start = i+1;
-                    int j = i+1;
-                    while( j < input.length() && Character.isDigit(input.charAt(j))) //assuming correct input
-                    {
-                        denString = input.substring(start,j+1);
-                        j++;
-                    }
-                }
-            
-                int num = Integer.parseInt(numString);
-                int den = denString.equals("") ? 1 : Integer.parseInt(denString);
-                
-                return new Fraction(num,den);
-            
-            }
+        String currentWord = "";
         
+        for (int i = 0; i <= input.length(); i++)
+        {
+            if (i == input.length() || input.charAt(i) == ' ') // evaluate full word
+            {
+                if (Character.isDigit(currentWord.charAt(0)) || (currentWord.length() > 1 && Character.isDigit(currentWord.charAt(1))))
+                    //its a number
+                {
+                    int fractionLocation =0, num=1,denom=1;
+                    
+                    for (int j =0; j < currentWord.length(); j++)
+                    {
+                        if (currentWord.charAt(j) == '/') fractionLocation=j;
+                    }
+                    
+                    if (fractionLocation == 0)
+                    {
+                        num = Integer.parseInt(currentWord);
+                    }
+                    else
+                    {
+                        num = Integer.parseInt(currentWord.substring(0,fractionLocation));
+                        denom = Integer.parseInt(currentWord.substring(fractionLocation + 1));
+                    }
+                    
+                    return new Fraction(num, denom);
+                
+                }
+                
+                currentWord="";
+            }
+            else currentWord += input.charAt(i);
+        }
+
+
         return null;
     }
 
