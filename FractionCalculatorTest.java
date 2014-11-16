4d4f6a2dcdb787671eee98f433a8f9cf3d512092
Paul Day <pday02@mail.bkk.ac.uk>
2014-11-16 15:50:22 +0000

    Added tests checking operator being held properly between runs

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -26,6 +26,15 @@ public class FractionCalculatorTest{
     if(fc.isIn("7",new String[]{"*"})) System.out.println("Error: isIn() test 5 failed");
     */
     
+    //test leading and trailing spaces
+    if (!fc.evaluate(new Fraction(-1,2), "  1/2  ").equals(new Fraction(1,2))) System.out.println("Error: leading spaces error");
+    
+    //test operator is stored between runs correctly
+    
+    fc.evaluate(new Fraction(-1,2), " 1/2 * "); //This should store a multiply in the calculator
+    if (!fc.evaluate(new Fraction(2,1), "3").equals(new Fraction(6,1))) System.out.println("Error: calculator not storing operator between runs");
+
+    
     //test addition
      if (!fc.evaluate(new Fraction(1,2), " + 1/3").equals(new Fraction(5,6))) System.out.println("Error: addition test 1 failed");
      if (!fc.evaluate(new Fraction(-1,2), " + -1/3").equals(new Fraction(-5,6))) System.out.println("Error: addition test 2 failed");
@@ -57,20 +66,32 @@ public class FractionCalculatorTest{
     if (!fc.evaluate(new Fraction(-1,2), " * 2 c").equals(new Fraction(0,1))) System.out.println("Error: clear test 2 failed");
     
     //test quit
-    if (!fc.evaluate(new Fraction(-1,2), " - 1/2 q + 5").equals(new Fraction(-1,1))) System.out.println("Error: quit test 1 failed");
-    
-    //test leading and trailing spaces
-    if (!fc.evaluate(new Fraction(-1,2), "  1/2  ").equals(new Fraction(1,2))) System.out.println("Error: leading spaces error");
+    if (!fc.evaluate(new Fraction(-1,2), " - 1/2 + q + 5").equals(new Fraction(-1,1))) System.out.println("Error: quit test 1 failed");
+    if (!fc.evaluate(new Fraction(1,2), "1/2").equals(new Fraction(1,2))) System.out.println("Error: quit test 2 failed. Operator not cleared.");
     
     //test error messages
     
-    //test values passed over from one run to another of iterate
+    System.out.println("The following three lines should have an error printed for having two consecutive operators:");
+    if (!fc.evaluate(new Fraction(-1,2), " 1/2 + 2 + * 3 *").equals(new Fraction(0,1))) System.out.println("Error: calculator not cleared on operator error");
+    if (!fc.evaluate(new Fraction(-1,2), " 1/2 + 2 + abs * 3 *").equals(new Fraction(0,1))) System.out.println("Error: calculator not cleared on operator error");
+    fc.evaluate(new Fraction(1,1),"*");
+    if (!fc.evaluate(new Fraction(-1,2), " / 1/2 *").equals(new Fraction(0,1))) System.out.println("Error: calculator not cleared on operator error");
+    if (!fc.evaluate(new Fraction(1,2), " 2").equals(new Fraction(2,1))) System.out.println("Error: operator not cleared when producing operator error");
     
+    
+
+    
+    //operator stored
     
     //test updateOperator
     
     
     // functions tested manually using main method
+    /*
+        - that it quit
+        - that it displays the propper greeting and leaving messages
+        - that it feeds in the correct fraction on evaluate method
+    */
     
     
     }
